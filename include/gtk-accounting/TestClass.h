831c8f12060e4e7d88af85598af17f5bc1e4a5f1
iShabang <curtin.shannon@gmail.com>
2020-04-07 15:40:19 -0700

    updated CMake project layout with test lib class

---
+++ include/gtk-accounting/TestClass.h
@@ -0,0 +1,13 @@
+#ifndef _GTK_ACCOUNTING_TEST_CLASS_H_
+#define _GTK_ACCOUNTING_TEST_CLASS_H_
+
+#include <iostream>
+#include <string>
+
+class TestClass
+{
+    public:
+    void print(const std::string &message);
+};
+
+#endif // _GTK_ACCOUNTING_TEST_CLASS_H_
---
+++ src/TestClass.cpp
@@ -0,0 +1,6 @@
+#include <gtk-accounting/TestClass.h>
+
+void TestClass::print(const std::string &message)
+{
+    std::cout << message << std::endl;
+}
