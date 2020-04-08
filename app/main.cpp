63bc08a2618252c637f1c93b72266af29ec31b58
iShabang <curtin.shannon@gmail.com>
2020-04-08 15:51:35 -0700

    initial LibFacade load in main

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,16 @@
 #include "Facade.h"
 
+#include <gtk-accounting/LibFacade.h>
 #include <iostream>
 #include <gtkmm/window.h>
 #include <gtkmm/application.h>
 
 int main(int argc, char **argv) {
 
+  // Initialize library
+  acc::LibFacade libFacade;
+
+  // Initialize gtk
   auto app = Gtk::Application::create(argc, argv, "gtk-accounting");
 
   Facade facade;
