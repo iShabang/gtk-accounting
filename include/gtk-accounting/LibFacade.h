2149969525a1eb1b68112169f344a6375d12e583
iShabang <curtin.shannon@gmail.com>
2020-04-10 01:45:51 -0700

    Added dispatch queue to facade

--- include/gtk-accounting/LibFacade.h
+++ include/gtk-accounting/LibFacade.h
@@ -1,28 +1,29 @@
 #ifndef _GTK_ACCOUNTING_LIB_FACADE_H_
 #define _GTK_ACCOUNTING_LIB_FACADE_H_
 
+#include <gtk-accounting/MainDispatcher.h>
 #include <gtk-accounting/SqliteImplementation.h>
 
 namespace acc {
 
 class LibFacade {
-
-  public:
+ public:
   LibFacade();
 
   ~LibFacade() = default;
   LibFacade(const LibFacade &) = delete;
   LibFacade(LibFacade &&) = delete;
-  LibFacade& operator=(const LibFacade &) = delete;
-  LibFacade& operator=(LibFacade &&) = delete;
+  LibFacade &operator=(const LibFacade &) = delete;
+  LibFacade &operator=(LibFacade &&) = delete;
 
-  DatabaseInterface& database();
+  DatabaseInterface &database();
+  DispatchInterface &dispatcher();
 
-  private:
+ private:
+  MainDispatcher m_dispatcher;
   SqliteImplementation m_database;
-
 };
 
-}
+}  // namespace acc
 
-#endif // _GTK_ACCOUNTING_LIB_FACADE_H_
+#endif  // _GTK_ACCOUNTING_LIB_FACADE_H_
--- src/LibFacade.cpp
+++ src/LibFacade.cpp
@@ -5,5 +5,6 @@ namespace acc {
 LibFacade::LibFacade() {}
 
 DatabaseInterface& LibFacade::database() { return m_database; }
+DispatchInterface& LibFacade::dispatcher() { return m_dispatcher; }
 
-}
+}  // namespace acc
