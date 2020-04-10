f0919db1bca67360aa71705b1d1ea7e40f3bb40d
iShabang <curtin.shannon@gmail.com>
2020-04-09 23:20:51 -0700

    added query strings

--- src/SqliteImplementation.cpp
+++ src/SqliteImplementation.cpp
@@ -7,10 +7,14 @@ namespace acc {
 
 static const std::string DB_NAME = "gtk-accounting.db";
 
-SqliteImplementation::SqliteImplementation() : m_isOpen(false) {
+static const std::string TRANS_HEAD =
+    "SELECT trans.id trans.name trans.date trans.amount FROM trans";
+static const std::string DATE_SUFFIX = "WHERE date >= ? AND date <= ?";
+static const std::string NAME_SUFFIX = "WHERE name LIKE ?";
+static const std::string AMOUNT_SUFFIX = "WHERE amount >= ? AND amount <= ?";
 
-  int status =
-      sqlite3_open_v2(DB_NAME.c_str(), &m_db, SQLITE_OPEN_READWRITE, nullptr);
+SqliteImplementation::SqliteImplementation() : m_isOpen(false) {
+  int status = sqlite3_open_v2(DB_NAME.c_str(), &m_db, SQLITE_OPEN_READWRITE, nullptr);
   if (status == SQLITE_CANTOPEN) {
     int flags = 0;
     flags |= SQLITE_OPEN_READWRITE;
@@ -24,17 +28,16 @@ SqliteImplementation::SqliteImplementation() : m_isOpen(false) {
   if (status == SQLITE_OK)
     m_isOpen = true;
   else
-    std::cerr << "SqliteImplementation::SqliteImplementation(): Error code: "
-              << status << " Message: " << sqlite3_errstr(status);
+    std::cerr << "SqliteImplementation::SqliteImplementation(): Error code: " << status
+              << " Message: " << sqlite3_errstr(status);
 }
 
-Result<DatabaseResult<Transaction>, std::string>
-SqliteImplementation::queryTransactions() {
+Result<DatabaseResult<Transaction>, std::string> SqliteImplementation::queryTransactions() {
   Result<DatabaseResult<Transaction>, std::string> result;
   sqlite3_stmt *statement = nullptr;
   std::string queryString = getQuery();
-  int resultCode = sqlite3_prepare_v2(
-      m_db, queryString.c_str(), queryString.length(), &statement, nullptr);
+  int resultCode =
+      sqlite3_prepare_v2(m_db, queryString.c_str(), queryString.length(), &statement, nullptr);
   if (resultCode == SQLITE_OK) {
     DatabaseResult<Transaction> transactions;
     int stepCode = sqlite3_step(statement);
@@ -42,10 +45,8 @@ SqliteImplementation::queryTransactions() {
       uint8_t index = 0;
       Transaction newTrans;
       newTrans.id = sqlite3_column_int64(statement, index++);
-      newTrans.name = reinterpret_cast<const char *>(
-          sqlite3_column_text(statement, index++));
-      newTrans.date = reinterpret_cast<const char *>(
-          sqlite3_column_text(statement, index++));
+      newTrans.name = reinterpret_cast<const char *>(sqlite3_column_text(statement, index++));
+      newTrans.date = reinterpret_cast<const char *>(sqlite3_column_text(statement, index++));
       newTrans.amount = sqlite3_column_double(statement, index);
       transactions.push_back(newTrans);
       stepCode = sqlite3_step(statement);
@@ -67,32 +68,30 @@ SqliteImplementation::queryTransactions() {
 bool SqliteImplementation::isOpen() const { return m_isOpen; }
 
 std::string SqliteImplementation::getQuery() {
-  // TODO
+  // TODO: Need filters to be implemented before querying the correct
+  // information
   std::string query;
   return query;
 }
 
-bool SqliteImplementation::insertTransactions(
-    std::vector<Transaction> transactions) {
+bool SqliteImplementation::insertTransactions(std::vector<Transaction> transactions) {
   bool result = true;
   sqlite3_stmt *statement = nullptr;
-  std::string queryString =
-      "INSERT INTO trans(name, date, amount) VALUES (?,?,?);";
-  int resultCode = sqlite3_prepare_v2(
-      m_db, queryString.c_str(), queryString.length(), &statement, nullptr);
+  std::string queryString = "INSERT INTO trans(name, date, amount) VALUES (?,?,?);";
+  int resultCode =
+      sqlite3_prepare_v2(m_db, queryString.c_str(), queryString.length(), &statement, nullptr);
   if (resultCode == SQLITE_OK) {
     bool status = true;
     for (auto &&i : transactions) {
-      status &= sqlite3_bind_text(statement, 1, i.name.c_str(), i.name.length(),
-                                  nullptr) == SQLITE_OK;
-      status &= sqlite3_bind_text(statement, 2, i.date.c_str(), i.date.length(),
-                                  nullptr) == SQLITE_OK;
+      status &=
+          sqlite3_bind_text(statement, 1, i.name.c_str(), i.name.length(), nullptr) == SQLITE_OK;
+      status &=
+          sqlite3_bind_text(statement, 2, i.date.c_str(), i.date.length(), nullptr) == SQLITE_OK;
       status &= sqlite3_bind_double(statement, 3, i.amount) == SQLITE_OK;
       resultCode = sqlite3_step(statement);
       if (resultCode != SQLITE_DONE) {
-        std::cerr << "SqliteImplementation::insertTransactions(): "
-                  << sqlite3_errstr(resultCode) << ". Code: " << resultCode
-                  << "\n";
+        std::cerr << "SqliteImplementation::insertTransactions(): " << sqlite3_errstr(resultCode)
+                  << ". Code: " << resultCode << "\n";
       }
       status &= resultCode == SQLITE_DONE;
       sqlite3_reset(statement);
@@ -105,20 +104,20 @@ bool SqliteImplementation::insertTransactions(
 
 void SqliteImplementation::createTables() {
   sqlite3_stmt *statement = nullptr;
-  std::string queryString = "CREATE TABLE IF NOT EXISTS trans ("
-                            "id INTEGER PRIMARY KEY,"
-                            "name TEXT,"
-                            "date TEXT,"
-                            "amount REAL);";
+  std::string queryString =
+      "CREATE TABLE IF NOT EXISTS trans ("
+      "id INTEGER PRIMARY KEY,"
+      "name TEXT,"
+      "date TEXT,"
+      "amount REAL);";
 
-  int resultCode = sqlite3_prepare_v2(
-      m_db, queryString.c_str(), queryString.length(), &statement, nullptr);
+  int resultCode =
+      sqlite3_prepare_v2(m_db, queryString.c_str(), queryString.length(), &statement, nullptr);
   if (resultCode != SQLITE_OK)
-    std::cerr << "SqliteImplementation::createTables(): Error: "
-              << sqlite3_errstr(resultCode)
+    std::cerr << "SqliteImplementation::createTables(): Error: " << sqlite3_errstr(resultCode)
               << ". sqlite error code: " << resultCode << "\n";
 
   sqlite3_finalize(statement);
 }
 
-} // namespace acc
+}  // namespace acc
