ad22be848dc52fe0c0c286d00c7886159d07c3db
iShabang <curtin.shannon@gmail.com>
2020-04-11 21:24:27 -0700

    adding LibFacadeInterface implementation

--- include/gtk-accounting/LibFacade.h
+++ include/gtk-accounting/LibFacade.h
@@ -1,12 +1,14 @@
 #ifndef _GTK_ACCOUNTING_LIB_FACADE_H_
 #define _GTK_ACCOUNTING_LIB_FACADE_H_
 
+#include <gtk-accounting/LibFacadeInterface.h>
 #include <gtk-accounting/MainDispatcher.h>
 #include <gtk-accounting/SqliteImplementation.h>
+#include <gtk-accounting/TransactionHandler.h>
 
 namespace acc {
 
-class LibFacade {
+class LibFacade : public LibFacadeInterface {
  public:
   LibFacade();
 
@@ -16,12 +18,15 @@ class LibFacade {
   LibFacade &operator=(const LibFacade &) = delete;
   LibFacade &operator=(LibFacade &&) = delete;
 
+ private:
   DatabaseInterface &database();
   DispatchInterface &dispatcher();
+  TransactionInterface &transaction();
 
  private:
   MainDispatcher m_dispatcher;
   SqliteImplementation m_database;
+  TransactionHandler m_transactions;
 };
 
 }  // namespace acc
--- src/LibFacade.cpp
+++ src/LibFacade.cpp
@@ -2,9 +2,10 @@
 
 namespace acc {
 
-LibFacade::LibFacade() {}
+LibFacade::LibFacade() : m_transactions(m_database,m_dispatcher) {}
 
 DatabaseInterface& LibFacade::database() { return m_database; }
 DispatchInterface& LibFacade::dispatcher() { return m_dispatcher; }
+TransactionInterface &LibFacade::transaction() { return m_transactions; }
 
 }  // namespace acc
