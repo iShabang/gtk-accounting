22c9031362f5cda766454d639f9e277c3e83cc42
iShabang <curtin.shannon@gmail.com>
2020-04-16 14:19:35 -0700

    using new Signal object

--- include/gtk-accounting/TransactionInterface.h
+++ include/gtk-accounting/TransactionInterface.h
@@ -2,17 +2,17 @@
 #define _GTK_ACCOUNTING_TRANSACTION_INTERFACE_H_
 
 #include <gtk-accounting/Transaction.h>
+#include <gtk-accounting/Signal.h>
 
 #include <vector>
-#include <sigc++/signal.h>
 
 namespace acc {
 class TransactionInterface {
 public:
   virtual ~TransactionInterface() {}
 
-  using TransactionsReceived = sigc::signal<void(std::vector<Transaction>)>;
-  using InsertFailed = sigc::signal<void()>;
+  using TransactionsReceived = Signal<void(std::vector<acc::Transaction>)>;
+  using InsertFailed = Signal<void()>;
 
   virtual void addTransaction(const Transaction &transaction) = 0;
   virtual void deleteSelected() = 0;
