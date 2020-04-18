aa33255128e6a2ddd363320e0f7bc2ae419b2707
iShabang <curtin.shannon@gmail.com>
2020-04-17 17:13:29 -0700

    Added table entries to the main table

--- app/Table.cpp
+++ app/Table.cpp
@@ -2,37 +2,51 @@
 
 #include <gtkmm/checkbutton.h>
 #include <gtkmm/label.h>
+
+#include <iostream>
 #include <string>
 
-Table::Table(acc::TransactionInterface &tran, Builder& builder) : m_tran(tran), m_builder(builder), m_tranConn(tran.transactionsReceived().connect([this](std::vector<acc::Transaction> data){ onTransactions(data); })) {
-  m_builder.getWidget("tableBox",m_tableBox);
+Table::Table(acc::TransactionInterface &tran, Builder &builder)
+    : m_tran(tran),
+      m_builder(builder),
+      m_tranConn(tran.transactionsReceived().connect(
+          [this](std::vector<acc::Transaction> data) { onTransactions(data); })) {
+  m_builder.getWidget("tableBox", m_tableBox);
   m_tran.requestTransactions();
 }
 
 void Table::onTransactions(std::vector<acc::Transaction> data) {
+  std::cout << "onTransactions. length: " << data.size() << std::endl;
   for (auto &&i : data) {
-    // create a horizontal box
-    Gtk::Box box;
-    box.set_halign(Gtk::ALIGN_FILL);
-    // add checkbox widget
-    Gtk::CheckButton checkButton;
-    checkButton.set_label("");
-    box.add(checkButton);
-
-    // add name label
-    Gtk::Label nameLabel;
-    nameLabel.set_label(i.name);
-    box.add(nameLabel);
-    // add date label
-    Gtk::Label dateLabel;
-    nameLabel.set_label(i.date);
-    box.add(dateLabel);
-
-    // add amount label
-    Gtk::Label amountLabel;
-    nameLabel.set_label(std::to_string(i.amount));
-    box.add(amountLabel);
-
-    m_tableBox->add(box);
+    Gtk::Box *box = createTableEntry(i);
+    m_tableBox->pack_start(*box);
+    m_tableBox->show_all_children();
   }
+}
+
+Gtk::Box *Table::createTableEntry(const acc::Transaction &transaction) {
+  Gtk::Box *box = Gtk::make_managed<Gtk::Box>();
+  Gtk::CheckButton *checkButton = Gtk::make_managed<Gtk::CheckButton>();
+  Gtk::Label *nameLabel = Gtk::make_managed<Gtk::Label>(transaction.name, Gtk::ALIGN_FILL);
+  Gtk::Label *dateLabel = Gtk::make_managed<Gtk::Label>(transaction.date, Gtk::ALIGN_FILL);
+  Gtk::Label *amountLabel =
+      Gtk::make_managed<Gtk::Label>(std::to_string(transaction.amount), Gtk::ALIGN_FILL);
+
+  box->set_halign(Gtk::ALIGN_FILL);
+  box->set_homogeneous(true);
+
+  checkButton->set_label("Select");
+  checkButton->set_halign(Gtk::ALIGN_FILL);
+  box->pack_start(*checkButton);
+
+  gtk_label_set_xalign(nameLabel->gobj(), 0);
+  box->pack_start(*nameLabel);
+
+  gtk_label_set_xalign(dateLabel->gobj(), 0);
+  box->pack_start(*dateLabel);
+
+  gtk_label_set_xalign(amountLabel->gobj(), 0);
+  box->pack_start(*amountLabel);
+
+  return box;
 }
--- app/Table.h
+++ app/Table.h
@@ -1,29 +1,26 @@
 #ifndef _GTK_ACCOUNTING_APP_TABLE_H_
 #define _GTK_ACCOUNTING_APP_TABLE_H_
 
-#include "Builder.h"
-
 #include <gtk-accounting/TransactionInterface.h>
-
 #include <gtkmm/box.h>
 
+#include "Builder.h"
+
 class Table {
+ public:
+  Table(acc::TransactionInterface &tran, Builder &builder);
 
-  public:
-    Table(acc::TransactionInterface &tran, Builder& builder); 
-
-  private:
-    void onEntrySelect();
-    void onTransactions(std::vector<acc::Transaction> data);
-    void onDelete();
-
-  private:
+ private:
+  void onEntrySelect();
+  void onTransactions(std::vector<acc::Transaction> data);
+  void onDelete();
+  Gtk::Box *createTableEntry(const acc::Transaction &transaction);
+   private:
     acc::TransactionInterface &m_tran;
     Builder &m_builder;
 
-
     Gtk::Box *m_tableBox;
     acc::ScopedConnection m_tranConn;
-};
+  };
 
-#endif // _GTK_ACCOUNTING_APP_TABLE_H_
+#endif  // _GTK_ACCOUNTING_APP_TABLE_H_
