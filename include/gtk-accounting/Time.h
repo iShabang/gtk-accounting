9dedb411ef4b4e6d6972009280109d208d21490f
iShabang <curtin.shannon@gmail.com>
2020-04-28 21:10:03 -0700

    added helper methods for date error checking

--- include/gtk-accounting/Time.h
+++ include/gtk-accounting/Time.h
@@ -1,6 +1,8 @@
 #ifndef _GTK_ACCOUNTING_TIME_H_
 #define _GTK_ACCOUNTING_TIME_H_
 
+#include <gtk-accounting/Result.h>
+
 #include <string>
 
 namespace acc {
@@ -10,8 +12,14 @@ namespace acc {
     int year;
   };
 
+  using DateResult = Result<Date,std::string>;
+
   std::string formatDate(const std::string date);
   Date getCurrentDate();
+  int getMonthDayCount(uint8_t month, uint32_t year);
+  bool isLeapYear(uint32_t year);
+  std::string dateToString(const Date &date);
+  DateResult stringToDate(const std::string &dateStr);
 }
 
 #endif // _GTK_ACCOUNTING_TIME_H_
--- src/Time.cpp
+++ src/Time.cpp
@@ -1,24 +1,75 @@
 #include <gtk-accounting/Time.h>
+#include <time.h>
 
 #include <sstream>
-#include <time.h>
+#include <iomanip>
 
 namespace acc {
 
-std::string formatDate(const std::string date)
-{
+std::string formatDate(const std::string date) {
   std::stringstream ss;
-  ss << date.substr(0,2) << "/";
-  ss << date.substr(2,2) << "/";
-  ss << date.substr(4,4);
+  ss << date.substr(0, 2) << "/";
+  ss << date.substr(2, 2) << "/";
+  ss << date.substr(4, 4);
   return ss.str();
 }
 
 Date getCurrentDate() {
   time_t now;
   time(&now);
-  struct tm * timeStruct = localtime(&now);
-  return { 1+timeStruct->tm_mon, timeStruct->tm_mday, timeStruct->tm_year + 1900};
+  struct tm* timeStruct = localtime(&now);
+  return {1 + timeStruct->tm_mon, timeStruct->tm_mday, timeStruct->tm_year + 1900};
 }
 
+int getMonthDayCount(uint8_t month, uint32_t year) {
+  int days = 0;
+
+  if (month < 8) {
+    if (month % 2 == 0) {
+      if (month == 2) {
+        days = isLeapYear(year) ? 29:28;
+      } else {
+        days = 30;
+      }
+
+    } else {
+      days = 31;
+    }
+
+  } else {
+    days = (month % 2 == 0) ? 31 : 30;
+  }
+
+  return days;
 }
+
+bool isLeapYear(uint32_t year) {
+  return year % 4 == 0;
+}
+
+std::string dateToString(const Date &date) {
+  std::stringstream ss;
+  ss << std::setfill('0');
+  ss << std::setw(2) << date.month;
+  ss << std::setw(2) << date.day;
+  ss << date.year;
+  return ss.str();
+}
+
+DateResult stringToDate(const std::string &dateStr) {
+
+  DateResult result;
+  if (dateStr.length() != 8) {
+    result.setError("invalid length. expected 8 characters");
+    return result;
+  }
+
+  Date date;
+  date.month = std::stoi(dateStr.substr(0,2));
+  date.day = std::stoi(dateStr.substr(2,2));
+  date.year = std::stoi(dateStr.substr(4,4));
+  result.setSuccess(std::move(date));
+  return result;
+}
+
+}  // namespace acc
