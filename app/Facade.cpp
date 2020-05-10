46aea31f2c7ee4f0a293cb0c44c97245212164e7
iShabang <curtin.shannon@gmail.com>
2020-05-10 15:18:26 -0700

    adding InfoBox to facade

--- app/Facade.cpp
+++ app/Facade.cpp
@@ -3,6 +3,9 @@
 Facade::Facade(acc::LibFacadeInterface &lib)
     : m_table(lib.transaction(), m_builder),
       m_addTranPopup(lib.transaction()),
-      m_menuBox(lib.transaction(), m_builder, m_addTranPopup) {}
+      m_menuBox(lib.transaction(), m_builder, m_addTranPopup),
+      m_infoBox(m_builder, lib.transaction())
+{
+}
 
 Builder &Facade::builder() { return m_builder; }
--- app/Facade.h
+++ app/Facade.h
@@ -1,26 +1,27 @@
 #ifndef _GTK_ACCOUNTING_APP_FACADE_H_
 #define _GTK_ACCOUNTING_APP_FACADE_H_
 
-#include "Builder.h"
-#include "Table.h"
-#include "AddTransactionPopup.h"
-#include "MenuBox.h"
-
 #include <gtk-accounting/LibFacadeInterface.h>
+
+#include "AddTransactionPopup.h"
+#include "Builder.h"
+#include "InfoBox.h"
+#include "MenuBox.h"
+#include "Table.h"
 
 class Facade
 {
-    public:
-    Facade(acc::LibFacadeInterface &lib);
+ public:
+  Facade(acc::LibFacadeInterface &lib);
 
-    Builder &builder();
+  Builder &builder();
 
-
-    private:
-    Builder m_builder;
-    Table m_table;
-    AddTransactionPopup m_addTranPopup;
-    MenuBox m_menuBox;
+ private:
+  Builder m_builder;
+  Table m_table;
+  AddTransactionPopup m_addTranPopup;
+  MenuBox m_menuBox;
+  InfoBox m_infoBox;
 };
 
-#endif // _GTK_ACCOUNTING_APP_FACADE_H_
+#endif  // _GTK_ACCOUNTING_APP_FACADE_H_
