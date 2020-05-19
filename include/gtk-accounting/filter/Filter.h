9523897078f6d08d299da2c4fbaefce47a6e1089
iShabang <curtin.shannon@gmail.com>
2020-05-19 16:02:03 -0700

    adding filter object

--- include/gtk-accounting/filter/Filter.h
+++ include/gtk-accounting/filter/Filter.h
@@ -1,10 +1,26 @@
 #ifndef _GTK_ACCOUNTING_FILTER_H_
 #define _GTK_ACCOUNTING_FILTER_H_
 
+#include <string>
+
 namespace acc
 {
   struct Filter
-  {};
+  {
+    bool hasDate;
+    bool hasAmount;
+    bool hasPattern;
+
+    std::string dateMin;
+    std::string dateMax;
+
+    std::string amountMin;
+    std::string amountMax;
+
+    std::string pattern;
+
+    std::string name;
+  };
 }
 
 #endif // _GTK_ACCOUNTING_FILTER_H_
