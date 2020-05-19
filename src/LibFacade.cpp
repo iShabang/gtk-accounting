a788e9007320979ce1a2382b6756734a094c73cc
iShabang <curtin.shannon@gmail.com>
2020-05-19 16:01:51 -0700

    adding filter parser

--- src/LibFacade.cpp
+++ src/LibFacade.cpp
@@ -1,11 +1,11 @@
 #include <gtk-accounting/facade/LibFacade.h>
 
-namespace acc {
-
-LibFacade::LibFacade() : m_transactions(m_database,m_dispatcher) {}
+namespace acc
+{
+LibFacade::LibFacade() : m_transactions(m_database, m_dispatcher), m_filterParser(m_dispatcher) {}
 
 DatabaseInterface& LibFacade::database() { return m_database; }
 DispatchInterface& LibFacade::dispatcher() { return m_dispatcher; }
-TransactionInterface &LibFacade::transaction() { return m_transactions; }
+TransactionInterface& LibFacade::transaction() { return m_transactions; }
 
 }  // namespace acc
